354936cbeccf81ff48388ea4b74cc67d48714783
Leonardo-bsb <leo.lisita@hotmail.com>
2020-06-23 17:28:41 -0300

    commit inicial

---
+++ step1/tutorial.cxx
@@ -0,0 +1,25 @@
+#include "TutorialConfig.h"
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        // report version
+        std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
+                  << Tutorial_VERSION_MINOR << std::endl;
+        std::cout << "Usage: " << argv[0] << " number" << std::endl;
+        return 1;
+    }
+
+    // convert input to double
+    const double inputValue = atof(argv[1]);
+
+    // calculate square root
+    const double outputValue = sqrt(inputValue);
+    std::cout << "The square root of " << inputValue << " is " << outputValue
+              << std::endl;
+}
